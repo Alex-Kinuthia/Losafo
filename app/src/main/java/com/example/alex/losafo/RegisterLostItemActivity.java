25759614fbafc0855125cab0797281f0510aff97
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-10-13 12:45:14 +0300

    posted lost items form

---
+++ app/src/main/java/com/example/alex/losafo/RegisterLostItemActivity.java
@@ -0,0 +1,13 @@
+package com.example.alex.losafo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterLostItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_lost_item);
+    }
+}
